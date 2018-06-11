1b82cd6c0286aedbdc578e0ba58feb7d59f335d5
Britney <britney.gibbs@student.montana.edu>
2018-06-11 15:02:56 -0600

    Create experiment and author for testing

--- braid/experiments/tests.py
+++ braid/experiments/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Author, Experiment, File
+
+def create_test_objects():
+    print("Do something!!!")
+    author_1 = Author(first_name='Jane', last_name='Doe')
+    author_1.save()
+    experiment_1 = Experiment(name='Metagenome',condition='acidic',author=author_1)
+    experiment_1.save()
+# 
 
 # Create your tests here.
