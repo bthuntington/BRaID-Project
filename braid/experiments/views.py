86825ce92e8d6de3cc5878fb45ae5e647164d9d8
Britney <britney.gibbs@student.montana.edu>
2018-06-11 09:58:05 -0600

    Automatically get Text mimetype

--- braid/experiments/views.py
+++ braid/experiments/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .forms import UploadFileForm
 from django.http import HttpResponse
+import magic
+from braid.settings import MEDIA_ROOT
 
 # view to upload files
 # uses UploadFileForm
@@ -9,11 +11,16 @@ def upload_file(request):
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
             file_model = form.save(commit=False)
+            
+            # read in file in chunk
+            handle_uploaded_file(request.FILES['file_file'])
+
             # automatically add path
             file_model.path = file_model.file_file.path
-           
-            # adds full path from root on computer
-            #print("File path: %s" % (file_model.path))
+
+            # get file type
+            if "text" in mimetype.lower():
+                file_model.mimetype = is_text(file_model.file_file.path)
 
             # Save to model
             file_model.save()
@@ -26,8 +33,19 @@ def upload_file(request):
     return render(request, 'experiments/upload_file.html', {'form': form})
 
 # write file in chuncks, not all at once
-def handle_uploaded_file(new_file, filename):
+def handle_uploaded_file(new_file):
     # ? store at path variable?
-    with open('files/' + filename, 'wb+') as destination:
+    filename = new_file.name
+    with open(MEDIA_ROOT + new_file.name, 'wb+') as destination:
         for chunk in new_file.chunks():
             destination.write(chunk)
+
+def is_text(path):
+    check_if_fasta(path)
+    # for now just return Text
+    return "Text"
+
+def check_if_fasta(text_file_path):
+    # TODO: use Biopython to determine if fasta file
+    pass
+
