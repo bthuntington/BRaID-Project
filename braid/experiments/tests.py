4b7fc966e01f854e93663f7fb74e3e2014c6f730
Britney <britney.gibbs@student.montana.edu>
2018-06-19 16:21:06 -0600

    Todo test for models

--- braid/experiments/tests.py
+++ braid/experiments/tests.py
@@ -1,12 +1,14 @@
 from django.test import TestCase
 from .models import Author, Experiment, File
 
+
 def create_test_objects():
     print("Do something!!!")
     author_1 = Author(first_name='Jane', last_name='Doe')
     author_1.save()
     experiment_1 = Experiment(name='Metagenome',condition='acidic',author=author_1)
     experiment_1.save()
-# 
 
-# Create your tests here.
+
+# TODO: Write test to make sure no more than one analysis model exists of
+# each type for a file at all times
