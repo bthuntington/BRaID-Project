798125d0aeeb5a5cd7f3e01c9acc769879e20b81
Britney <britney.gibbs@student.montana.edu>
2018-06-19 10:06:43 -0600

    Added none option

--- braid/experiments/utils.py
+++ braid/experiments/utils.py
@@ -2,6 +2,6 @@
 def set_analysis_options(file_type):
     type_list = []
     if 'csv' in file_type.lower():
-        type_list = ['Bayesian Network']
+        type_list = ['Bayesian Network', 'None']
 
     return type_list
